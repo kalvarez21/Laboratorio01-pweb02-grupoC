9b7d4cb5615ce8821d48523c5cf1fcec606ae9a0
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-04-28 22:38:35 -0500

    Funcion suma - Prueba01

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,12 @@
 class Calculadora{
 	public static void main(String [] args){
-		
+		int a, b;
+		a = 20;
+		b = 10;
+		System.out.println("La suma de : " + a + " + " + b + " = " + add(a,b));
 	}
-	int add(int a, int b){return 0;}
-	int sub(int a, int b){return 0;}
+	public static int add(int a, int b){return a+b;}
+	public static int sub(int a, int b){return 0;}
 	int mul(int a, int b){return 0;}
 	int div(int a, int b){return 0;}
 	int mod(int a, int b){return 0;}
