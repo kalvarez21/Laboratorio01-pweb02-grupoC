d99ccb764ee3add1c441577f3719bd8dcb454c67
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-04-28 22:28:09 -0500

    Funcion Division - Prueba01

--- Calculadora.java
+++ Calculadora.java
@@ -4,10 +4,12 @@ class Calculadora{
 		x = 15;
 		y = 3;
 		System.out.println("El producto de: "+ x +" * " + y +" = " + mul(x,y));
+		System.out.println("El cociente de: "+ x +" / " + y +" = " + div(x,y));
+
 	}
 	int add(int a, int b){return 0;}
 	int sub(int a, int b){return 0;}
 	public static int mul(int a, int b){return a*b;}
-	int div(int a, int b){return 0;}
-	int mod(int a, int b){return 0;}
+	public static int div(int a, int b){return a/b;}
+	public static int mod(int a, int b){return 0;}
 }
