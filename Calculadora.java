7f2f7897f443f80d65e9671472a3d94f199122cf
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-04-28 22:41:31 -0500

    Funcion Resta - prueba 01

--- Calculadora.java
+++ Calculadora.java
@@ -4,9 +4,10 @@ class Calculadora{
 		a = 20;
 		b = 10;
 		System.out.println("La suma de : " + a + " + " + b + " = " + add(a,b));
+		System.out.println("La resta de : " + a + " - " + b + " = " + sub(a,b));
 	}
 	public static int add(int a, int b){return a+b;}
-	public static int sub(int a, int b){return 0;}
+	public static int sub(int a, int b){return a-b;}
 	int mul(int a, int b){return 0;}
 	int div(int a, int b){return 0;}
 	int mod(int a, int b){return 0;}
